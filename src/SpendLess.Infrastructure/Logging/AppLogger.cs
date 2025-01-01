7556745782d8ad4158b7a413ce9b55fda9510f88
anarahmadov <anar.axmed5514@gmail.com>
2025-01-01 23:11:02 +0400

    add logging for the layers apart from core business logic

---
+++ src/SpendLess.Infrastructure/Logging/AppLogger.cs
@@ -0,0 +1,34 @@
+﻿using SpendLess.Application.Common;
+using Microsoft.Extensions.Logging;
+
+namespace SpendLess.Infrastructure.Logging
+{
+    public class AppLogger<T> : IAppLogger<T> where T : class
+    {
+        private readonly ILogger<T> _logger;
+        public AppLogger(ILogger<T> logger)
+        {
+            _logger = logger; 
+        }
+
+        public void LogCritical(string message, params object[] args)
+        {
+            _logger.LogCritical(message, args);
+        }
+
+        public void LogError(string message, params object[] args)
+        {
+            _logger.LogError(message, args);
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+    }
+}
